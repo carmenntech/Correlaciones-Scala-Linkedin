2bbafe9cd9d023a6b0e8c75c27a2adf0576fc7f7
carmenpatit <carmentorrecilla6@gmail.com>
2024-09-12 11:23:39 +0200

    Extracion de los items de scala e iportacion al mongo local

---
+++ Busqueda-keyword-users.py
@@ -0,0 +1,60 @@
+from linkedin_api import Linkedin
+from credenciales import *
+import json
+import pandas as pd
+from linkedin_api.client import Client
+import pandas as pd
+from pymongo import MongoClient
+
+from linkedin_api.utils.helpers import (
+    get_id_from_urn,
+    get_urn_from_raw_update,
+    get_list_posts_sorted_without_promoted,
+    parse_list_raw_posts,
+    parse_list_raw_urns,
+    generate_trackingId,
+    generate_trackingId_as_charString,
+)
+# Authenticate using any Linkedin account credentials
+api = Linkedin(USER, PWD)
+
+
+
+users_linkedin = api.search_people(keywords = 'Scala')
+
+
+# Conectar a MongoDB
+#client = MongoClient("mongodb://localhost:27017/")
+#db = client["linkedinapi"]
+#collection = db["items-scala"]
+
+df_people = pd.DataFrame(users_linkedin)
+df_urn = df_people["urn_id"]
+urn_values = list(df_urn)
+rows = []
+
+# Iterar sobre los resultados e imprimirlos
+for urn in urn_values:
+
+    resultados = api.get_profile_skills(urn_id = urn)
+
+    # Extraer solo los valores del campo 'name'
+    lista_items = [item['name'] for item in resultados]
+
+    # Añadir un nuevo diccionario con los datos a la lista de filas
+    rows.append({
+        "urn": urn,
+        "list_items": lista_items,
+    })
+
+# Convertir la lista de filas en un DataFrame
+df = pd.DataFrame(rows)
+
+# Conectar a MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["linkedinapi"]
+collection = db["items-scala"]
+
+# Insertar los datos en la colección
+collection.insert_many(df.to_dict("records"))
+
